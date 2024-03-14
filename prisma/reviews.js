df640afdae136e3c40b55292b845f7165fe52a64
unknown <williamevans001001@gmail.com>
2024-03-13 20:28:16 -0400

    wrote express routes

--- prisma/reviews.js
+++ prisma/reviews.js
@@ -16,9 +16,62 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res) => {
   try {
+    const id = +req.params.id;
+    const review = await prisma.review.findUnique({
+      where: { id },
+    });
+
+    if (!review) {
+      return next({
+        status: 404,
+        message: "Review #${id} doesn't exist.",
+      });
+    }
+
+    res.json(review);
   } catch {
     next({
       message: "The server failed to reach the database.",
     });
   }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, product } = req.body;
+    const review = await prisma.review.create({
+      data: {
+        name,
+        product,
+      },
+    });
+
+    res.json(review);
+  } catch {
+    next({
+      message: "The server failed to reach the database.",
+    });
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const id = +req.params.id;
+    const review = await prisma.review.findUnique({
+      where: { id },
+    });
+
+    if (!review) {
+      return next({
+        status: 404,
+        message: "Review #${id} doesn't exist.",
+      });
+    }
+
+    await prisma.review.delete({
+      where: { id },
+    });
+
+    res.sendStatus(201);
+  } catch {}
 });
